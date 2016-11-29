13550c2dba6801224fe2c747e0f2b840ceacb0fd
Simon Lehericey <simon.lehericey@gmail.com>
2016-11-29 18:25:23 +0100

    Add episodes

--- app/services/series-service.js
+++ app/services/series-service.js
@@ -2,7 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   series: [
-    { id: 1, thumbnailSrc: 'http://www.bdtheque.com/repupload/T/T_52401.JPG', title: 'taipi' },
+    { 
+      id: 1, 
+      thumbnailSrc: 'http://www.bdtheque.com/repupload/T/T_52401.JPG', 
+      title: 'taipi',
+      episodes: [
+        { number: 1, title: 'great' },
+        { number: 2, title: 'crazy' },
+        { number: 3, title: 'dingo' },
+        { number: 4, title: 'foufou' },
+        { number: 5, title: 'whaou' },
+      ] 
+    },
     { id: 2, thumbnailSrc: 'http://www.bdtheque.com/repupload/T/T_52401.JPG', title: 'taipi' }
   ],
   getAll() { return this.get('series'); },
