bac4fb754a2520d5b235dee6066dde5b7d09c7d9
Maxime Arnstamm <Maxime@Maxime.Maxime>
2016-12-01 19:40:33 +0100

    style pour la page principale / titres à retoucher

--- app/services/series-service.js
+++ app/services/series-service.js
@@ -2,9 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   series: [
-    { 
-      id: 1, 
-      thumbnailSrc: 'http://www.bdtheque.com/repupload/T/T_52401.JPG', 
+    {
+      id: 1,
+      thumbnailSrc: 'http://www.bdtheque.com/repupload/T/T_52401.JPG',
       title: 'taipi',
       episodes: [
         { number: 1, title: 'great' },
@@ -12,9 +12,12 @@ export default Ember.Service.extend({
         { number: 3, title: 'dingo' },
         { number: 4, title: 'foufou' },
         { number: 5, title: 'whaou' },
-      ] 
+      ]
     },
-    { id: 2, thumbnailSrc: 'http://www.bdtheque.com/repupload/T/T_52401.JPG', title: 'taipi' }
+    { id: 2, thumbnailSrc: 'http://www.bdtheque.com/repupload/T/T_52401.JPG', title: 'taipi' },
+    { id: 3, thumbnailSrc: 'http://www.bdtheque.com/repupload/T/T_52401.JPG', title: 'taipi' },
+    { id: 4, thumbnailSrc: 'http://www.bdtheque.com/repupload/T/T_52401.JPG', title: 'taipi' },
+    { id: 5, thumbnailSrc: 'http://www.bdtheque.com/repupload/T/T_52401.JPG', title: 'taipi' }
   ],
   getAll() { return this.get('series'); },
   getById(id) { return this.get('series').findBy('id', id); }
